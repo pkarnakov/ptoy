5f56427b67c870904e6be1e43d56ad0bc321347e
Petr Karnakov <kpetr@ethz.ch>
2020-09-02 00:09:54 +0200

    gl 3.1

--- source/main.cpp
+++ source/main.cpp
@@ -295,7 +295,7 @@ int main() {
   init();
 
   SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
   SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 
   SDL_Window* window = SDL_CreateWindow(
