66b49dc65b614fd134286339caa72730c29a727c
Petr Karnakov <kpetr@ethz.ch>
2020-09-01 18:25:00 +0200

    shaders wip

---
+++ source/logger.h
@@ -0,0 +1,49 @@
+// Created by Petr Karnakov on 25.02.2020
+
+#include <string>
+#include <stdexcept>
+#include <sstream>
+
+#define FILELINE \
+  (std::string() + __FILE__ + ":" + std::to_string(__LINE__))
+
+// force assert
+#define fassert_1(x)                                                      \
+  do {                                                                    \
+    if (!(x)) {                                                           \
+      throw std::runtime_error(FILELINE + ": assertion failed '" #x "'"); \
+    }                                                                     \
+  } while (0);
+
+#define fassert_2(x, msg)                                    \
+  do {                                                       \
+    if (!(x)) {                                              \
+      throw std::runtime_error(                              \
+          FILELINE + ": assertion failed '" #x "'\n" + (msg)); \
+    }                                                        \
+  } while (0);
+
+#define GET_COUNT(_1, _2, _3, COUNT, ...) COUNT
+#define VA_SIZE(...) GET_COUNT(__VA_ARGS__, 3, 2, 1, 0)
+#define PTOY_CAT(x, y) x##y
+#define PTOY_XCAT(x, y) PTOY_CAT(x, y)
+
+#define fassert(...) \
+  PTOY_XCAT(fassert##_, VA_SIZE(__VA_ARGS__))(__VA_ARGS__)
+
+#define NAMEVALUE(x)                 \
+  ([&]() -> std::string {            \
+    std::stringstream namevalue_s;   \
+    namevalue_s << #x << '=' << (x); \
+    return namevalue_s.str();        \
+  }())
+
+#define fassert_equal(x, y)                                                   \
+  do {                                                                        \
+    if (!((x) == (y))) {                                                      \
+      std::stringstream fasrteq_s;                                            \
+      fasrteq_s << FILELINE << ": assertion failed, expected equal ";      \
+      fasrteq_s << #x << "='" << (x) << "' and " << #y << "='" << (y) << "'"; \
+      throw std::runtime_error(fasrteq_s.str());                              \
+    }                                                                         \
+  } while (0);
--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 
+#include <GL/glew.h>
 #include <SDL.h>
+#include <SDL_opengl.h>
 #include "game.hpp"
+#include "logger.h"
 
 #include <atomic>
 #include <chrono>
@@ -31,6 +34,11 @@ std::atomic<bool> pause;
 
 using std::cout;
 using std::endl;
+
+GLuint gProgramID = 0;
+GLint gVertexPos2DLocation = -1;
+GLuint gVBO = 0;
+GLuint gIBO = 0;
 
 int frame_number;
 
@@ -105,6 +113,20 @@ void display(void) {
 
   glFlush();
 
+  glUseProgram(gProgramID);
+  glEnableVertexAttribArray(gVertexPos2DLocation);
+
+  glBindBuffer(GL_ARRAY_BUFFER, gVBO);
+  glVertexAttribPointer(
+      gVertexPos2DLocation, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GLfloat), NULL);
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, gIBO);
+  glDrawElements(GL_TRIANGLE_FAN, 4, GL_UNSIGNED_INT, NULL);
+
+  glDisableVertexAttribArray(gVertexPos2DLocation);
+
+  glUseProgram(0);
+
   // glutSwapBuffers();
 
   flag_display = false;
@@ -156,6 +178,59 @@ vect GetDomainMousePosition() {
   return GetDomainMousePosition(x, y);
 }
 
+void printShaderLog(GLuint shader) {
+  // Make sure name is shader
+  if (glIsShader(shader)) {
+    // Shader log length
+    int infoLogLength = 0;
+    int maxLength = infoLogLength;
+
+    // Get info string length
+    glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &maxLength);
+
+    // Allocate string
+    char* infoLog = new char[maxLength];
+
+    // Get info log
+    glGetShaderInfoLog(shader, maxLength, &infoLogLength, infoLog);
+    if (infoLogLength > 0) {
+      // Print Log
+      printf("%s\n", infoLog);
+    }
+
+    // Deallocate string
+    delete[] infoLog;
+  } else {
+    printf("Name %d is not a shader\n", shader);
+  }
+}
+void printProgramLog(GLuint program) {
+  // Make sure name is shader
+  if (glIsProgram(program)) {
+    // Program log length
+    int infoLogLength = 0;
+    int maxLength = infoLogLength;
+
+    // Get info string length
+    glGetProgramiv(program, GL_INFO_LOG_LENGTH, &maxLength);
+
+    // Allocate string
+    char* infoLog = new char[maxLength];
+
+    // Get info log
+    glGetProgramInfoLog(program, maxLength, &infoLogLength, infoLog);
+    if (infoLogLength > 0) {
+      // Print Log
+      printf("%s\n", infoLog);
+    }
+
+    // Deallocate string
+    delete[] infoLog;
+  } else {
+    printf("Name %d is not a program\n", program);
+  }
+}
+
 int main() {
   if (SDL_Init(SDL_INIT_VIDEO) != 0) {
     SDL_Log("Unable to initialize SDL: %s\n", SDL_GetError());
@@ -179,6 +254,100 @@ int main() {
   glClearColor(gray, gray, gray, 0.0);
   glClear(GL_COLOR_BUFFER_BIT);
   SDL_GL_SwapWindow(window);
+
+  glewInit();
+
+  auto shd = [&] {
+    gProgramID = glCreateProgram();
+    // Create vertex shader
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+    // Get vertex source
+    const GLchar* vertexShaderSource[] = {
+        R"EOF(#version 140
+in vec2 LVertexPos2D;
+void main() {
+  float x = LVertexPos2D.x;
+  float y = LVertexPos2D.y;
+  gl_Position = vec4(x, x > 0 ? y : y - 0.3, 0, 1);
+}
+)EOF"};
+
+    // Set vertex source
+    glShaderSource(vertexShader, 1, vertexShaderSource, NULL);
+
+    // Compile vertex source
+    glCompileShader(vertexShader);
+
+    // Check vertex shader for errors
+    GLint vShaderCompiled = GL_FALSE;
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &vShaderCompiled);
+    if (vShaderCompiled != GL_TRUE) {
+      printf("Unable to compile vertex shader %d!\n", vertexShader);
+      printShaderLog(vertexShader);
+      fassert(false);
+    }
+
+    // Attach vertex shader to program
+    glAttachShader(gProgramID, vertexShader);
+
+    // Create fragment shader
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+    // Get fragment source
+    const GLchar* fragmentShaderSource[] = {
+        R"EOF(#version 140
+out vec4 LFragment;
+void main() {
+  LFragment = vec4(.0, 1.0, 1.0, 1.0 ); 
+}
+)EOF"};
+
+    // Set fragment source
+    glShaderSource(fragmentShader, 1, fragmentShaderSource, NULL);
+
+    // Compile fragment source
+    glCompileShader(fragmentShader);
+
+    // Check fragment shader for errors
+    GLint fShaderCompiled = GL_FALSE;
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &fShaderCompiled);
+    if (fShaderCompiled != GL_TRUE) {
+      printf("Unable to compile fragment shader %d!\n", fragmentShader);
+      printShaderLog(fragmentShader);
+      fassert(false);
+    }
+    glAttachShader(gProgramID, fragmentShader);
+    glLinkProgram(gProgramID);
+    GLint programSuccess = GL_TRUE;
+    glGetProgramiv(gProgramID, GL_LINK_STATUS, &programSuccess);
+    if (programSuccess != GL_TRUE) {
+      printProgramLog(gProgramID);
+      printf("Error linking program %d!\n", gProgramID);
+      fassert(false);
+    }
+
+    // Get vertex attribute location
+    gVertexPos2DLocation = glGetAttribLocation(gProgramID, "LVertexPos2D");
+    if (gVertexPos2DLocation == -1) {
+      printf("LVertexPos2D is not a valid glsl program variable!\n");
+      fassert(false);
+    }
+
+    // Initialize clear color
+    glClearColor(0.f, 0.f, 0.f, 1.f);
+    GLfloat vertexData[] = {-0.5f, -0.5f, 0.5f, -0.5f, 0.5f, 0.5f, -0.5f, 0.5f};
+    GLuint indexData[] = {0, 1, 2, 3};
+    glGenBuffers(1, &gVBO);
+    glBindBuffer(GL_ARRAY_BUFFER, gVBO);
+    glBufferData(
+        GL_ARRAY_BUFFER, 2 * 4 * sizeof(GLfloat), vertexData, GL_STATIC_DRAW);
+    glGenBuffers(1, &gIBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, gIBO);
+    glBufferData(
+        GL_ELEMENT_ARRAY_BUFFER, 4 * sizeof(GLuint), indexData, GL_STATIC_DRAW);
+  };
+  shd();
 
   G = std::unique_ptr<game>(new game(width, height));
   std::thread computation_thread(cycle);
