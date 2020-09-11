48df78e66af7d4875abad95d4bab0b132decbc44
Petr Karnakov <kpetr@ethz.ch>
2020-09-11 18:53:48 +0200

    add identity geom.glsl

--- shaders/frag.glsl
+++ shaders/frag.glsl
@@ -3,15 +3,15 @@
 uniform uint pointSize;
 uniform sampler2D myTex;
 
-flat in uvec2 upos;
-flat in float colorf;
+flat in uvec2 geom_upos;
+flat in float geom_colorf;
 
 out vec4 fragColor;
 
 void main() {
-  vec2 p = gl_FragCoord.xy - upos;
+  vec2 p = gl_FragCoord.xy - geom_upos;
   float d = dot(p, p) / (pointSize * pointSize);
-  vec4 c = vec4(colorf, 0, 0, 1 - pow(d, 4));
-  c.y = texture(myTex, (p / pointSize + 1) * 0.5).x * colorf;
+  vec4 c = vec4(geom_colorf, 0, 0, 1 - pow(d, 4));
+  c.y = texture(myTex, (p / pointSize + 1) * 0.5).x * geom_colorf;
   fragColor = c;
 }
--- shaders/geom.glsl
+++ shaders/geom.glsl
@@ -1,5 +1,8 @@
 #version 330
 
+// FIXME: this is an identity shader, it copies input to output.
+// Without it, 'lines.geom.glsl' is not executed while drawing lines.
+
 layout (points) in;
 flat in uvec2 upos[];
 flat in float colorf[];
@@ -10,6 +13,9 @@ flat out float geom_colorf;
 
 void main() {
   gl_Position = gl_in[0].gl_Position;
+  gl_PointSize = gl_in[0].gl_PointSize;
+  geom_upos = upos[0];
+  geom_colorf = colorf[0];
   EmitVertex();
   EndPrimitive();
 }
